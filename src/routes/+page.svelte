2688768dc8000fa6d945a98a002fcc4dc320fd7a
Eleazar Odivilas <eleazar.odivilas01@gmail.com>
2023-05-29 15:48:38 +0800

    feature: beautify css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,22 +71,22 @@
 				<button on:click={() => setOperation('divide')}>&divide;</button>
 			</div>
 			<div class="numbers">
-				<div>
+				<div class="top">
 					<button on:click={() => setValue('7')}>7</button>
 					<button on:click={() => setValue('8')}>8</button>
 					<button on:click={() => setValue('9')}>9</button>
 				</div>
-				<div>
+				<div class="middle">
 					<button on:click={() => setValue('4')}>4</button>
 					<button on:click={() => setValue('5')}>5</button>
 					<button on:click={() => setValue('6')}>6</button>
 				</div>
-				<div>
+				<div class="bottom">
 					<button on:click={() => setValue('1')}>1</button>
 					<button on:click={() => setValue('2')}>2</button>
 					<button on:click={() => setValue('3')}>3</button>
 				</div>
-				<div>
+				<div class="extra">
 					<button on:click={() => setValue('0')}>0</button>
 					<button on:click={() => setValue('.')}>.</button>
 					<button on:click={() => setValue('C')}>C</button>
@@ -112,14 +112,24 @@
 		width: 100vw;
 	}
 	.calculator {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 5px;
 		width: 300px;
-		height: 300px;
+		height: 288px;
 		border: 1px solid #eee;
 		box-shadow: 2px 2px 2px #eee;
 		padding: 10px;
+		background-color: #232b2b;
+		border-radius: 10px;
+	}
+	.buttons {
+		height: 240px;
+		width: 100%;
 	}
 	.calculator input {
-		width: 85.5%;
+		width: 85.6%;
 		padding: 20px;
 		outline: none;
 		text-align: right;
@@ -133,29 +143,35 @@
 		display: flex;
 		justify-content: space-between;
 		width: 100%;
+		height: 39.5px;
+		margin-bottom: 5px;
 	}
 	.calculator .buttons .operations button {
-		width: 24%;
+		width: 23.6%;
 	}
 	.calculator .buttons .numbers {
-		width: 75%;
+		width: 74.5%;
+		height: 80%;
 	}
 	.calculator .buttons .numbers > div {
 		display: flex;
 		justify-content: space-between;
 	}
 	.calculator .buttons .numbers > div button {
-		width: 32%;
+		width: 31.5%;
+		height: 39.5px;
+		margin-bottom: 5px;
 	}
 	.calculator .equal {
-		flex: 1;
+		width: 25%;
 	}
 	.calculator .equal button {
-		margin-left: 5%;
+		margin-left: 7%;
 		width: 95%;
-		height: 95%;
-		background: #00acee;
+		height: 98%;
+		background: #fd4e02;
 		color: #eee;
+		border-radius: 4px;
 	}
 	.calculator button {
 		outline: none;
